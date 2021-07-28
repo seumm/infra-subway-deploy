9dfab29d8f8fd261d63012abe4decf4b36c5dfe9
seumm <ganu104205@gmail.com>
2021-07-28 21:08:15 +0900

    commit

--- frontend/src/views/main/MainPage.vue
+++ frontend/src/views/main/MainPage.vue
@@ -5,7 +5,7 @@
       <p class="mt-2 text-gray-700">
         RunningMap Service 페이지입니다. <br />
         제공되는 템플릿을 활용해 프로젝트를 완성해 보세요!
-        version2
+        version3
       </p>
       <ul class="text-blue-700">
         <li v-for="navItem in navItems" :key="navItem._id" class="inline-block p-1">
